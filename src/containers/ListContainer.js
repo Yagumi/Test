95fb049991649f00e48b62bb7fa09ce5c44a9ef5
Yagumi <yagumi24@gmail.com>
2019-11-08 18:54:29 +0300

    Delete ListContainer.js

--- src/containers/ListContainer.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-import List from '../components/List';
-
-const ListContainer = ({ list }) => {
-  return <MenuList list={list} />;
-};
-
-export default ListContainer;
